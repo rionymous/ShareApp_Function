5cdc983c6b318d56f1bd8f75d47c92e984bd550b
rionymous_ <callmerion24@gmail.com>
2019-10-20 15:44:50 +0700

    Initial commit

---
+++ app/src/main/java/com/androidrion/shareapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.androidrion.shareapp;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.share) {
+            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBodyText = "Check it out. Your message goes here";
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject here");
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBodyText);
+            startActivity(Intent.createChooser(sharingIntent, "Sharing Option"));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
